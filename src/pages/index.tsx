cf6c6b59c18c4d16f1d36f8b025c5e4be305ac30
ryansinbad <ryan@sinbad.co.id>
2024-08-21 11:03:02 +0700

    fix: add index.tsx files for main/default page

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { useGetIssues } from '@/services/queries';
+import { GetServerSideProps } from 'next';
+import React from 'react';
+
+const IssuesList: React.FC = () => {
+  const { data, isLoading, isError } = useGetIssues({ enabled: true });
+
+  console.log(data, isLoading, isError);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Issues List</h1>
+      <ul className="space-y-4">
+      </ul>
+    </div>
+  );
+};
+
+export default IssuesList;
