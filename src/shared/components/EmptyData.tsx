e85a90a31786a0df539c07961ae94faa9afee5a2
ryansinbad <ryan@sinbad.co.id>
2024-08-23 21:48:41 +0700

    chore: add data-testid

--- src/shared/components/EmptyData.tsx
+++ src/shared/components/EmptyData.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './EmptyData.css';
 
 interface Props {
+  testId?: string;
   title: string;
 }
 
 export const EmptyData: React.FC<Props> = (props) => (
   <div className="empty-data-container">
-    <p className="empty-data-text">{props.title}</p>
+    <p
+      className="empty-data-text"
+      data-testid={`empty-data-text.${props.testId}`}
+    >
+      {props.title}
+    </p>
   </div>
 );
