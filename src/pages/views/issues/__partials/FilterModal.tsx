33e6b2e12b54232e00cddf095dcee0e16e50dc5b
ryansinbad <ryan@sinbad.co.id>
2024-08-24 00:50:05 +0700

    refactor: separate FilterModal logic and layout

--- src/pages/views/issues/__partials/FilterModal.tsx
+++ src/pages/views/issues/__partials/FilterModal.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import { useState, useEffect, use } from 'react';
 import { Dropdown, Button } from '@/shared/components';
 import './FilterModal.css';
+import { useFilterModalUtil } from './FilterModal.util';
 
 export interface IFilterData {
   sortBy: string;
   order: string;
 }
-interface Props {
+export interface Props {
+  testId?: string;
   isOpen: boolean;
   onClose: () => void;
   onConfirm: (filters: IFilterData) => void;
+  onReset: () => void;
+  filterSortBy: string;
+  filterOrder: string;
 }
 
 const FilterModal: React.FC<Props> = (props) => {
-  const [sortBy, setSortBy] = React.useState('');
-  const [order, setOrder] = React.useState('');
+  const { sortBy, setSortBy, order, setOrder, handleResetFilter } =
+    useFilterModalUtil(props);
 
   if (!props.isOpen) return null;
 
@@ -33,6 +38,7 @@ const FilterModal: React.FC<Props> = (props) => {
               <div className="filter-modal-title">
                 <h3>Filter Issues</h3>
                 <Dropdown
+                  testId={`${props.testId}.sort-by-dropdown`}
                   label="Sort By"
                   value={sortBy}
                   onChange={(val) => setSortBy(val)}
@@ -44,6 +50,7 @@ const FilterModal: React.FC<Props> = (props) => {
                   defaultOption="Select Sort Criteria"
                 />
                 <Dropdown
+                  testId={`${props.testId}.order-dropdown`}
                   label="Order"
                   value={order}
                   onChange={(val) => setOrder(val)}
@@ -58,6 +65,7 @@ const FilterModal: React.FC<Props> = (props) => {
           </div>
           <div className="filter-modal-footer">
             <Button
+              testId={`${props.testId}.apply-button`}
               type="text"
               title="Apply"
               onClick={() => props.onConfirm({ sortBy, order })}
@@ -65,18 +73,17 @@ const FilterModal: React.FC<Props> = (props) => {
             />
             <div className="filter-modal-footer-buttons">
               <Button
+                testId={`${props.testId}.close-button`}
                 type="text"
                 title="Close"
                 onClick={props.onClose}
                 backgroundColor="neutral"
               />
               <Button
+                testId={`${props.testId}.reset-button`}
                 type="text"
                 title="Reset"
-                onClick={() => {
-                  setSortBy('');
-                  setOrder('');
-                }}
+                onClick={handleResetFilter}
                 backgroundColor="yellow"
               />
             </div>
