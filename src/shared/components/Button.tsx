b07af1354bc8a1634c0b3a79da86c9479599373e
ryansinbad <ryan@sinbad.co.id>
2024-08-23 16:28:45 +0700

    refactor: css and itself implementation

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Button.css';
+
+interface TextButtonProps {
+  type: 'text';
+  title: string;
+  backgroundColor: 'blue' | 'red' | 'green' | 'yellow' | 'neutral';
+  icon?: never;
+  onClick: () => void;
+  isDisabled?: boolean;
+}
+
+interface IconButtonProps {
+  type: 'icon';
+  title?: never;
+  backgroundColor: 'blue' | 'red' | 'green' | 'yellow' | 'neutral';
+  icon: React.ReactNode;
+  onClick: () => void;
+  isDisabled?: boolean;
+}
+
+type ButtonProps = TextButtonProps | IconButtonProps;
+
+export const Button: React.FC<ButtonProps> = (props) => {
+  let colorClass = props.backgroundColor;
+  if (props.isDisabled) {
+    colorClass += ' disabled';
+  }
+
+  let padding = 'button-padding-text';
+  if (props.type === 'icon') {
+    padding = 'button-padding-icon';
+  }
+
+  return (
+    <div>
+      <button
+        onClick={props.isDisabled ? undefined : props.onClick}
+        className={`button-container ${colorClass} ${padding}`}
+        disabled={props.isDisabled}
+      >
+        {props.type === 'icon' && props.icon
+          ? React.isValidElement(props.icon)
+            ? React.cloneElement(props.icon as React.ReactElement)
+            : null
+          : props.title}
+      </button>
+    </div>
+  );
+};
