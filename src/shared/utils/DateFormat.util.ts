f03e12fb38365927f5fec143b5b8b7716c09115a
ryansinbad <ryan@sinbad.co.id>
2024-08-23 21:27:28 +0700

    chore: add validation for DateFormat if the params isnt string date

--- src/shared/utils/DateFormat.util.ts
+++ src/shared/utils/DateFormat.util.ts
@@ -1,10 +1,13 @@
-// Will return a formatted date string in the format of "DD MMM, YY".
+// Will return a formatted date string in the format of "MMM DD, YY".
 export const simpleDateFormat = (value: string) => {
   if (!value) {
     return null;
   }
 
   const date = new Date(value);
+  if (isNaN(date.getTime())) {
+    return 'Invalid Date';
+  }
 
   return new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
