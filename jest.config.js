53d989dbd2a3eb925f53b6acacf26d9e3fef26f8
ryansinbad <ryan@sinbad.co.id>
2024-08-24 23:28:06 +0700

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '^@/components/(.*)$': '<rootDir>/src/shared/components/$1',
     '^@/utils/(.*)$': '<rootDir>/src/shared/utils/$1',
-    '^@/shared/components/(.*)$': '<rootDir>/src/shared/components/$1', // Ensure this path is mapped
-    '^@/services/(.*)$': '<rootDir>/src/services/$1', // Add this path if needed
+    '^@/shared/components/(.*)$': '<rootDir>/src/shared/components/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
